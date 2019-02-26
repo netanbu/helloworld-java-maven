7ae834a01ce2b9bb8a372f29c70bc9b9d5b02670
netanbu <netanbu@gmail.com>
2019-02-27 00:06:27 +0530

    DCC-1
    DCC-1

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World DCC-1" );
+        System.out.println( "Hello World DCC-1 --3d" );
     }
 }
