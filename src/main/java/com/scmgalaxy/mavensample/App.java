3c1d433eceeb70715b1fbe13e2dcf979d13758be
netanbu <netanbu@gmail.com>
2019-02-28 12:43:26 +0530

    updated some comments

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World DCC-1 --3d" );
+        System.out.println( "Hello World to DCC-2 --3d" );
     }
 }
