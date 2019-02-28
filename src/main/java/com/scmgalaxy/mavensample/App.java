0d827291911e99343da40f51912d4c8880bbb8e7
netanbu <netanbu@gmail.com>
2019-02-28 12:53:50 +0530

    DCC-2
    test

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World to DCC-1 --3d" );
+        System.out.println( "Hello Worlds to DCC-1 --3d" );
     }
 }
